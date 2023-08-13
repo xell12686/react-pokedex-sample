16b6cec567a541c229713653c8abe480589cd24a
jdev <jlacandula@gmail.com>
2023-08-14 06:21:27 +0800

    chore: replace raw with basics

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the online pokedex heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = screen.getByRole('heading', { name: /the online pokedex/i });
+  expect(headingElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="Header">
+        <h1>The Online Pokedex</h1>
       </header>
+      <main>
+        <section>
+          <p>Content here...</p>
+        </section>
+      </main>
+      <footer className='Footer'>
+        <p>Copyright &copy; 2023 React Code Challenge. All Rights Reserved.</p>
+        Footer
+      </footer>
     </div>
   );
 }
