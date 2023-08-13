1ce5063397f7658cc6c36219f97cbb10e154deff
jdev <jlacandula@gmail.com>
2023-08-14 07:34:37 +0800

    feat: use fetch hook on App root

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,26 @@ import './App.css';
 import Header from './components/Header';
 import Content from './components/Content';
 import Footer from './components/Footer';
+import useFetchPokemon from './utils/useFetchPokemon';
 
 function App() {
+  const { data, loading, error } = useFetchPokemon('/data/pokemon.json'); // Use the custom hook to fetch Pokémon data
+
   return (
     <div className="App">
-
       <Header />
-      <Content />
+      <main>
+        <section>
+          {loading ? (
+            <div>Loading...</div> // Loading message
+          ) : error ? (
+            <div>Error: {error}</div> // Error message
+          ) : (
+            <Content pokemonData={data?.entries} /> // Pass Pokémon data to the Content component
+          )}
+        </section>
+      </main>
       <Footer />
-
     </div>
   );
 }
